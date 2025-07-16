485bb4521ed690ddc28236944d869facc1d81125
Digitalworld808 <raisubhakaran5@gmail.com>
2025-07-16 17:02:52 +0530

    Added Home.jsx with banner

---
+++ src/pages/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <img
+        src="https://source.unsplash.com/1200x400/?village,hills,forest"
+        alt="Hamro Badamtam Banner"
+        style={{ width: "100%", height: "auto", borderRadius: "10px" }}
+      />
+      <h1>Hamro Badamtam</h1>
+      <p>स्वागत छ! यहाँ बदमतामको मुख्य जानकारी पाउनुहुनेछ।</p>
+    </div>
+  );
+}
